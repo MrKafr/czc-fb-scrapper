4ef248e934338ae3e00ee35584603c1ae89c6eee
MrKafr <stefke.lukas@gmail.com>
2023-11-17 14:13:00 +0100

    aaaa

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-const { Client } = require("discord.js");
-const config = require("./config.json");
-
 const SCRAP_FB = require("./scrap-fb.js");
-const INTERVAL = 120000
+//const open = (...args) => import('open').then(({ default: fetch }) => fetch(...args));
+const INTERVAL = 30000;
 
-// Create a new client instance
-const client = new Client({
-  intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"],
-  partials: ["CHANNEL"],
-});
-
-// When the client is ready, this only runs once
-client.once("ready", async () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-
-  setInterval(async function () {
-    await SCRAP_FB()
-  }, INTERVAL);
-});
-
-
-client.login(config.discordToken);
-
-module.exports = { client };
+(async function () {
+    let current, previous;
+    setInterval(async function () {
+        current = await SCRAP_FB()
+        console.count()
+        if (current && previous && current !== previous) {
+            console.log("https://www.facebook.com/czc.cz");
+            console.log(current);
+            console.log("NEW");
+            //open('https://www.facebook.com/czc.cz');
+            ///open(current);
+        }
+        if (current) {
+            previous = current
+        }
+    }, INTERVAL);
+})();
